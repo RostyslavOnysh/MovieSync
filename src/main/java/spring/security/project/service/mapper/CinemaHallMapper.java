a30ea1211ca40720db4b6a47ea6031d562741055
Ros <traine41@icloud.com>
2023-08-17 18:46:42 +0100

    6. mapper and DTO classes

---
+++ src/main/java/spring/security/project/service/mapper/CinemaHallMapper.java
@@ -0,0 +1,27 @@
+package spring.security.project.service.mapper;
+
+import spring.security.project.dto.request.CinemaHallRequestDto;
+import spring.security.project.dto.response.CinemaHallResponseDto;
+import spring.security.project.model.CinemaHall;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CinemaHallMapper implements RequestDtoMapper<CinemaHallRequestDto, CinemaHall>,
+        ResponseDtoMapper<CinemaHallResponseDto, CinemaHall> {
+    @Override
+    public CinemaHall mapToModel(CinemaHallRequestDto dto) {
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setDescription(dto.getDescription());
+        cinemaHall.setCapacity(dto.getCapacity());
+        return cinemaHall;
+    }
+
+    @Override
+    public CinemaHallResponseDto mapToDto(CinemaHall cinemaHall) {
+        CinemaHallResponseDto responseDto = new CinemaHallResponseDto();
+        responseDto.setId(cinemaHall.getId());
+        responseDto.setCapacity(cinemaHall.getCapacity());
+        responseDto.setDescription(cinemaHall.getDescription());
+        return responseDto;
+    }
+}
