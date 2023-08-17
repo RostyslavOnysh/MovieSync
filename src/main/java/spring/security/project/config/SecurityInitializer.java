367fc31b2050211ea7c14aca805e98b85ef14983
Ros <traine41@icloud.com>
2023-08-17 18:48:20 +0100

    7. Config files and custom exception
    pom.xml contains checkstyle
    use
    ```bash
    mvn clean package
    ```

---
+++ src/main/java/spring/security/project/config/SecurityInitializer.java
@@ -0,0 +1,6 @@
+package spring.security.project.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
