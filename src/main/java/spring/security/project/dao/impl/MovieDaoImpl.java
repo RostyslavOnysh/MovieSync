c005318438cfbb5de97c603156875a80410d6ca3
Ros <traine41@icloud.com>
2023-08-17 18:43:15 +0100

    4. DAO

---
+++ src/main/java/spring/security/project/dao/impl/MovieDaoImpl.java
@@ -0,0 +1,14 @@
+package spring.security.project.dao.impl;
+
+import spring.security.project.dao.AbstractDao;
+import spring.security.project.dao.MovieDao;
+import spring.security.project.model.Movie;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MovieDaoImpl extends AbstractDao<Movie> implements MovieDao {
+    public MovieDaoImpl(SessionFactory factory) {
+        super(factory, Movie.class);
+    }
+}
